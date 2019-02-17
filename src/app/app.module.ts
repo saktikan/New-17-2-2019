7da31abca89ab291267adcae48bc02fe57d50252
saktikan <saktikan02@gmail.com>
2019-02-17 10:45:32 +0700

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 
+imports: [BrowserModule,FormsModule,ReactiveFormsModule];
 @NgModule({
   declarations: [
     AppComponent
