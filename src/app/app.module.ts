188e7f392c0549b8311068a8ec30c491d276e647
saktikan <saktikan02@gmail.com>
2019-02-17 10:56:46 +0700

    Error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { DetailComponent } from './detail/detail.component';
+import { FormComponent } from './form/form.component';
 
 imports: [BrowserModule,FormsModule,ReactiveFormsModule];
 @NgModule({
   declarations: [
     AppComponent,
-    DetailComponent
+    DetailComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
